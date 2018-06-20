3277355050e28f1dc9b194d6085b729ac240184f
ERIC KELLY <eakelly220@gmail.com>
2018-06-20 16:18:16 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,7 +26,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors
    JOIN series ON series.author_id = books.id
    JOIN subgenres ON subgenres.id = series.subgenre_id
-   GROUP BY author
+   GROUP BY authors
   "end
 
 def select_series_title_with_most_human_characters
