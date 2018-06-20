db15185b147ac80fd88e6f779c24d867b5569c79
ERIC KELLY <eakelly220@gmail.com>
2018-06-20 16:56:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
    JOIN books ON books.series_id = series.id
    JOIN character_books ON character_books.book_id = books.id
-   JOIN characters ON character.id = character_books.character_id
+   JOIN characters ON characters.id = character_books.character_id
    WHERE characters.species = 'human'
    GROUP BY series.title
   "
