93645bf89383dc3ad2cb1f9d15b92603d6e9e074
ERIC KELLY <eakelly220@gmail.com>
2018-06-20 16:12:54 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT species, count(species) FROM characters
    GROUP BY species
-   ORDER BY species DESC
+   ORDER BY species DESC LIMIT 1
   "
 end
 
